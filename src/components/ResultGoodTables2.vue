1af5ff959617e0e77c81bc2e4958e9917df955b4
receptopalak <receptopalak@gmail.com>
2022-06-08 15:22:14 +0300

    commit

--- src/components/ResultGoodTables2.vue
+++ src/components/ResultGoodTables2.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <vue-good-table
+     ref="my-table"
       mode="remote"
       v-on:page-change="onPageChange"
       v-on:sort-change="onSortChange"
       v-on:column-filter="onColumnFilter"
       v-on:per-page-change="onPerPageChange"
+      v-on:selected-rows-change="selectionChanged"
       :totalRows="countData"
       :totalPage="totalPage"
       :pagination-options="{
@@ -16,7 +18,7 @@
         nextLabel: 'Sonraki',
         prevLabel: 'Önceki',
         rowsPerPageLabel: 'Sayfadaki Satır Sayısı',
-        ofLabel: 'of',
+        ofLabel: '/',
         pageLabel: 'page', // for 'pages' mode
         allLabel: 'All',
         dropdownAllowAll: false,
@@ -25,6 +27,17 @@
       :columns="columns"
       :sort-options="{
         enabled: true,
+      }"
+      theme="nocturnal"
+      styleClass="vgt-table striped"
+      :select-options="{
+        enabled: true,
+        selectOnCheckboxOnly: true, // only select when checkbox is clicked instead of the row
+        selectionInfoClass: 'custom-class',
+        selectionText: 'rows selected',
+        clearSelectionText: 'clear',
+        disableSelectInfo: true, // disable the select info panel on top
+        selectAllByGroup: true, // when used in combination with a grouped table, add a checkbox in the header row to check/uncheck the entire group
       }"
     />
   </div>
@@ -154,11 +167,31 @@ export default {
         }
       );
     },
+    selectionChanged() {
+      // Seçilen satırları aşağıdaki şekilde yakalaya biliyoruz. 
+    //  console.log( this.$refs["my-table"].selectedRows)
+    },
   },
 };
 </script>
 <style >
 .vgt-responsive {
   max-height: 60vh;
+}
+.vgt-table.nocturnal thead th {
+  color: #c7ced8;
+  border-bottom: 2px solid #9b9b9b;
+  background: linear-gradient(#000000, #2c394f);
+}
+#vgt-table > thead > tr > th.vgt-checkbox-col {
+  background: linear-gradient(#000000, #2c394f);
+  border-bottom: 2px solid #9b9b9b;
+  color: #c7ced8;
+}
+
+.vgt-wrap.nocturnal .vgt-wrap__footer {
+  color: #c7ced8;
+  border-bottom: 2px solid #9b9b9b;
+  background: linear-gradient(#2c394f, #000000);
 }
 </style>
